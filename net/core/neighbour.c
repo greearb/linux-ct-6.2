greearb/linux-ct-6.1

neigh: random backoff for neigh (arp) retransmits.
Allow random backoffs for neighbour (ARP) retransmits.
This helps reduce the thundering herd when using lots of
MAC-vlans, for instance.

Signed-off-by: Ben Greear <greearb@candelatech.com>

net/core/neighbour.c
@@ -1027,6 +1027,14 @@ static __inline__ int neigh_max_probes(struct neighbour *n)
 	        NEIGH_VAR(p, MCAST_PROBES));
 }
 
+static unsigned long neigh_rand_retry(struct neighbour* neigh) {
+	int b = NEIGH_VAR(neigh->parms, RETRANS_RAND_BACKOFF_MS);
+	if (b) {
+		return prandom_u32_max(b);
+	}
+	return 0;
+}
+
 static void neigh_invalidate(struct neighbour *neigh)
 	__releases(neigh->lock)
 	__acquires(neigh->lock)
@@ -1121,11 +1129,12 @@ static void neigh_timer_handler(struct timer_list *t)
 			atomic_set(&neigh->probes, 0);
 			notify = 1;
 			next = now + max(NEIGH_VAR(neigh->parms, RETRANS_TIME),
-					 HZ/100);
+					 HZ/100) + neigh_rand_retry(neigh);
 		}
 	} else {
 		/* NUD_PROBE|NUD_INCOMPLETE */
-		next = now + max(NEIGH_VAR(neigh->parms, RETRANS_TIME), HZ/100);
+		next = now + max(NEIGH_VAR(neigh->parms, RETRANS_TIME), HZ/100)
+			+ neigh_rand_retry(neigh);
 	}
 
 	if ((neigh->nud_state & (NUD_INCOMPLETE | NUD_PROBE)) &&
@@ -3740,6 +3749,7 @@ static struct neigh_sysctl_table {
 		NEIGH_SYSCTL_ZERO_INTMAX_ENTRY(PROXY_QLEN, "proxy_qlen"),
 		NEIGH_SYSCTL_USERHZ_JIFFIES_ENTRY(ANYCAST_DELAY, "anycast_delay"),
 		NEIGH_SYSCTL_USERHZ_JIFFIES_ENTRY(PROXY_DELAY, "proxy_delay"),
+		NEIGH_SYSCTL_ZERO_INTMAX_ENTRY(RETRANS_RAND_BACKOFF_MS, "retrans_rand_backoff_ms"),
 		NEIGH_SYSCTL_USERHZ_JIFFIES_ENTRY(LOCKTIME, "locktime"),
 		NEIGH_SYSCTL_UNRES_QLEN_REUSED_ENTRY(QUEUE_LEN, QUEUE_LEN_BYTES, "unres_qlen"),
 		NEIGH_SYSCTL_MS_JIFFIES_REUSED_ENTRY(RETRANS_TIME_MS, RETRANS_TIME, "retrans_time_ms"),
@@ -3818,6 +3828,8 @@ int neigh_sysctl_register(struct net_device *dev, struct neigh_parms *p,
 		t->neigh_vars[NEIGH_VAR_BASE_REACHABLE_TIME].proc_handler = handler;
 		/* RetransTime (in milliseconds)*/
 		t->neigh_vars[NEIGH_VAR_RETRANS_TIME_MS].proc_handler = handler;
+		/* RetransRandBackoffTime (in milliseconds)*/
+		t->neigh_vars[NEIGH_VAR_RETRANS_RAND_BACKOFF_MS].proc_handler = handler;
 		/* ReachableTime (in milliseconds) */
 		t->neigh_vars[NEIGH_VAR_BASE_REACHABLE_TIME_MS].proc_handler = handler;
 	} else {

greearb/linux-ct-6.2

net/core/neighbour.c
@@ -1058,6 +1058,14 @@ static __inline__ int neigh_max_probes(struct neighbour *n)
 	        NEIGH_VAR(p, MCAST_PROBES));
 }
 
+static unsigned long neigh_rand_retry(struct neighbour* neigh) {
+	int b = NEIGH_VAR(neigh->parms, RETRANS_RAND_BACKOFF_MS);
+	if (b) {
+		return get_random_u32_below(b);
+	}
+	return 0;
+}
+
 static void neigh_invalidate(struct neighbour *neigh)
 	__releases(neigh->lock)
 	__acquires(neigh->lock)
@@ -1152,11 +1160,12 @@ static void neigh_timer_handler(struct timer_list *t)
 			atomic_set(&neigh->probes, 0);
 			notify = 1;
 			next = now + max(NEIGH_VAR(neigh->parms, RETRANS_TIME),
-					 HZ/100);
+					 HZ/100) + neigh_rand_retry(neigh);
 		}
 	} else {
 		/* NUD_PROBE|NUD_INCOMPLETE */
-		next = now + max(NEIGH_VAR(neigh->parms, RETRANS_TIME), HZ/100);
+		next = now + max(NEIGH_VAR(neigh->parms, RETRANS_TIME), HZ/100)
+			+ neigh_rand_retry(neigh);
 	}
 
 	if ((neigh->nud_state & (NUD_INCOMPLETE | NUD_PROBE)) &&
@@ -3754,6 +3763,7 @@ static struct neigh_sysctl_table {
 		NEIGH_SYSCTL_ZERO_INTMAX_ENTRY(PROXY_QLEN, "proxy_qlen"),
 		NEIGH_SYSCTL_USERHZ_JIFFIES_ENTRY(ANYCAST_DELAY, "anycast_delay"),
 		NEIGH_SYSCTL_USERHZ_JIFFIES_ENTRY(PROXY_DELAY, "proxy_delay"),
+		NEIGH_SYSCTL_ZERO_INTMAX_ENTRY(RETRANS_RAND_BACKOFF_MS, "retrans_rand_backoff_ms"),
 		NEIGH_SYSCTL_USERHZ_JIFFIES_ENTRY(LOCKTIME, "locktime"),
 		NEIGH_SYSCTL_UNRES_QLEN_REUSED_ENTRY(QUEUE_LEN, QUEUE_LEN_BYTES, "unres_qlen"),
 		NEIGH_SYSCTL_MS_JIFFIES_REUSED_ENTRY(RETRANS_TIME_MS, RETRANS_TIME, "retrans_time_ms"),
@@ -3832,6 +3842,8 @@ int neigh_sysctl_register(struct net_device *dev, struct neigh_parms *p,
 		t->neigh_vars[NEIGH_VAR_BASE_REACHABLE_TIME].proc_handler = handler;
 		/* RetransTime (in milliseconds)*/
 		t->neigh_vars[NEIGH_VAR_RETRANS_TIME_MS].proc_handler = handler;
+		/* RetransRandBackoffTime (in milliseconds)*/
+		t->neigh_vars[NEIGH_VAR_RETRANS_RAND_BACKOFF_MS].proc_handler = handler;
 		/* ReachableTime (in milliseconds) */
 		t->neigh_vars[NEIGH_VAR_BASE_REACHABLE_TIME_MS].proc_handler = handler;
 	} else {
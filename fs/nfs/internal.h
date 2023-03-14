greearb/linux-ct-3.14

nfs: Add srcaddr member to nfs_client.
Signed-off-by: Ben Greear <greearb@candelatech.com>

fs/nfs/internal.h
@@ -99,6 +99,11 @@ struct nfs_parsed_mount_data {
 	struct {
 		struct sockaddr_storage	address;
 		size_t			addrlen;
+	} srcaddr;
+
+	struct {
+		struct sockaddr_storage address;
+		size_t                  addrlen;
 		char			*hostname;
 		u32			version;
 		int			port;

greearb/linux-ct-6.2

fs/nfs/internal.h
@@ -115,6 +115,14 @@ struct nfs_fs_context {
 			struct sockaddr_storage	_address;
 		};
 		size_t			addrlen;
+	} srcaddr;
+
+	struct {
+		union {
+			struct sockaddr address;
+			struct sockaddr_storage _address;
+		};
+		size_t                  addrlen;
 		char			*hostname;
 		u32			version;
 		int			port;